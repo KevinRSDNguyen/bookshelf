d557a896ca43a4a840d9e003c6ac47a8b40dd7fd
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-04-13 13:31:19 -0700

    created route for users to log out

--- server/models/user.js
+++ server/models/user.js
@@ -33,15 +33,15 @@ const userSchema = mongoose.Schema({
   }
 });
 
-userSchema.pre('save', function(next){
+userSchema.pre('save', function (next) {
   var user = this;
 
-  if(user.isModified('password')) {
-    bcrypt.genSalt(SALT_I, function(err, salt){
-      if(err) return next(err);
+  if (user.isModified('password')) {
+    bcrypt.genSalt(SALT_I, function (err, salt) {
+      if (err) return next(err);
 
-      bcrypt.hash(user.password, salt, function(err,hash){
-        if(err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
         user.password = hash;
         next();
       });
@@ -51,23 +51,41 @@ userSchema.pre('save', function(next){
   }
 });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb){
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
-    if(err) return cb(err);
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
     cb(null, isMatch);
   })
 };
 
-userSchema.methods.generateToken = function(cb){
+userSchema.methods.generateToken = function (cb) {
   var user = this;
   var token = jwt.sign(user._id.toHexString(), config.SECRET);
 
   user.token = token;
-  user.save(function(err, user){
-    if(err) return cb(err);
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+  jwt.verify(token, config.SECRET, function (err, decode) {
+    User.findOne({ _id: decode, token }, (err, user) => {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+}
+
+userSchema.methods.deleteToken = function (cb) {
+  var user = this;
+
+  user.update({ $unset: { token: 1 } }, (err, user) => {
+    if (err) return cb(err);
     cb(null, user);
   });
 }
 
 const User = mongoose.model('User', userSchema);
-module.exports = {User};
+module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -8,18 +8,26 @@ const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE);
 
-const {User} = require('./models/user');
-const {Book} = require('./models/book');
+const { User } = require('./models/user');
+const { Book } = require('./models/book');
+const { auth } = require('./middleware/auth');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // GET //
+app.get('/api/logout', auth, (req, res) => {
+  req.user.deleteToken((err, user) => {
+    if(err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+});
+
 app.get('/api/getBook', (req, res) => {
   let id = req.query.id;
 
   Book.findById(id, (err, doc) => {
-    if(err) return res.status(400).send(err);
+    if (err) return res.status(400).send(err);
     res.send(doc);
   });
 });
@@ -28,20 +36,20 @@ app.get('/api/books', (req, res) => {
   // localhost:3001/api/books?skip=3&limit=2&order=asc
   let skip = parseInt(req.query.skip);
   let limit = parseInt(req.query.limit);
-  let {order} = req.query;
+  let { order } = req.query;
 
   //ORDER = asc || desc
-  Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err, doc) => {
-    if(err) return res.status(400).send(err);
+  Book.find().skip(skip).sort({ _id: order }).limit(limit).exec((err, doc) => {
+    if (err) return res.status(400).send(err);
     res.send(doc);
   });
 });
 
 app.get('/api/getReviewer', (req, res) => {
-  let id= req.query.id;
+  let id = req.query.id;
 
   User.findById(id, (err, doc) => {
-    if(err || !doc) return res.status(400).send(err || 'User not found');
+    if (err || !doc) return res.status(400).send(err || 'User not found');
     res.json({
       name: doc.name,
       last: doc.lastname
@@ -51,8 +59,15 @@ app.get('/api/getReviewer', (req, res) => {
 
 app.get('/api/users', (req, res) => {
   User.find({}, (err, users) => {
-    if(err) return res.status(400).send(err);
+    if (err) return res.status(400).send(err);
     res.status(200).send(users);
+  });
+});
+
+app.get('/api/user_posts', (req, res) => {
+  Book.find({ ownerId: req.query.user }, (err, docs) => {
+    if (err) return res.status(400).send(err);
+    res.send(docs);
   });
 });
 
@@ -61,7 +76,7 @@ app.post('/api/book', (req, res) => {
   const book = new Book(req.body);
 
   book.save((err, doc) => {
-    if(err) return res.status(400).send(err);
+    if (err) return res.status(400).send(err);
     res.status(200).json({
       post: true,
       bookId: doc._id
@@ -73,7 +88,7 @@ app.post('/api/register', (req, res) => {
   const user = new User(req.body);
 
   user.save((err, doc) => {
-    if(err) return res.json({success:false});
+    if (err) return res.json({ success: false });
     res.status(200).json({
       success: true,
       user: doc
@@ -82,17 +97,17 @@ app.post('/api/register', (req, res) => {
 });
 
 app.post('/api/login', (req, res) => {
-  User.findOne({email: req.body.email}, (err, user) => {
-    if(!user) return res.json({isAuth: false, message: "Auth failed, email not found"});
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) return res.json({ isAuth: false, message: "Auth failed, email not found" });
 
     user.comparePassword(req.body.password, (err, isMatch) => {
-      if(!isMatch) return res.json({
+      if (!isMatch) return res.json({
         isAuth: false,
         message: "Wrong password"
       });
 
       user.generateToken((err, user) => {
-        if(err) return res.status(400).send(err);
+        if (err) return res.status(400).send(err);
         res.cookie('auth', user.token).json({
           isAuth: true,
           id: user._id,
@@ -105,8 +120,8 @@ app.post('/api/login', (req, res) => {
 
 // UPDATE //
 app.post('/api/book_update', (req, res) => {
-  Book.findByIdAndUpdate(req.body._id, req.body, {new:true}, (err, doc) => {
-    if(err) return res.status(400).send(err);
+  Book.findByIdAndUpdate(req.body._id, req.body, { new: true }, (err, doc) => {
+    if (err) return res.status(400).send(err);
     res.json({
       success: true,
       doc
@@ -119,7 +134,7 @@ app.delete('/api/delete_book', (req, res) => {
   const id = req.query.id;
 
   Book.findByIdAndRemove(id, (err, doc) => {
-    if(err) return res.status(400).send(err);
+    if (err) return res.status(400).send(err);
     res.json(true);
   });
 });
